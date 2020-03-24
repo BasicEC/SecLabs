c798deb7c4c30a683dfd2539f0bc7ab4d4a5a58c
v.zakusilo <v.zakusilo@studiomobile.ru>
2020-03-24 14:52:38 +0300

    lab 1 and lab 3 from part 1 of Oziganov's book
    task variant 3

---
+++ SecLab1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SecLab1
+{
+    public class Program
+    {
+        private const string Abc = "abcdefghiklmnopqrstuvwxyz"; // no j.
+        private static readonly char[,] Matrix = new char[5, 5];
+        private const string FilePath = "/home/basicec/text.txt";
+
+        
+        public static void Main(string[] args)
+        {
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine($"Cannot find file {Path.GetFullPath(FilePath)}");
+                return;
+            }
+
+            using var sr = new StreamReader(FilePath);
+            var message = sr.ReadToEnd();
+
+            var used = new HashSet<char>();
+            Console.WriteLine("Write key:");
+            var key = Console.ReadLine();
+            if (string.IsNullOrEmpty(key)) Console.WriteLine("Key cannot be null or empty.");
+            key = Normalize(key);
+
+            var i = 0;
+            var j = 0;
+            while (!string.IsNullOrEmpty(key))
+            {
+                used.Add(key[0]);
+                Matrix[i, j] = key[0];
+                if (j == 4) i++;
+                j = (j + 1) % 5;
+                key = key.Replace($"{key[0]}", "");
+            }
+
+            foreach (var letter in Abc.Where(letter => !used.Contains(letter)))
+            {
+                Matrix[i, j] = letter;
+                if (j == 4) i++;
+                j = (j + 1) % 5;
+            }
+
+            Console.WriteLine($"Message: {message}");
+            message = Normalize(message);
+
+            if (message.Length % 2 == 1) message = $"{message}x";
+
+            Encrypt(ref message);
+            Console.WriteLine($"Encrypted: {message}");
+
+            Decrypt(ref message);
+            Console.WriteLine($"Decrypted: {message}");
+        }
+
+        private static string Normalize(string input) => input?
+            .Trim()
+            .ToLowerInvariant()
+            .Replace(" ", "")
+            .Replace("\t", "")
+            .Replace("j", "i");
+
+        private static void Encrypt(ref string message)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < message.Length; i += 2)
+            {
+                var first = message[i];
+                var second = message[i + 1];
+                if (first == second) second = 'x';
+                IndexOf(first, out var firstI, out var firstJ);
+                IndexOf(second, out var secondI, out var secondJ);
+                if (firstI == secondI) //same line
+                {
+                    first = Matrix[firstI, (firstJ + 1) % 5];
+                    second = Matrix[secondI, (secondJ + 1) % 5];
+                }
+                else if (firstJ == secondJ)
+                {
+                    first = Matrix[(firstI + 1) % 5, firstJ];
+                    second = Matrix[(secondI + 1) % 5, secondJ];
+                }
+                else
+                {
+                    first = Matrix[firstI, secondJ];
+                    second = Matrix[secondI, firstJ];
+                }
+
+                builder.AppendFormat("{0}{1}", first, second);
+            }
+
+            message = builder.ToString();
+        }
+
+        private static void Decrypt(ref string message)
+        {
+            var builder = new StringBuilder();
+            for (var i = 0; i < message.Length; i += 2)
+            {
+                var first = message[i];
+                var second = message[i + 1];
+                IndexOf(first, out var firstI, out var firstJ);
+                IndexOf(second, out var secondI, out var secondJ);
+                if (firstI == secondI) //same line
+                {
+                    first = Matrix[firstI, (firstJ + 4) % 5];
+                    second = Matrix[secondI, (secondJ + 4) % 5];
+                }
+                else if (firstJ == secondJ)
+                {
+                    first = Matrix[(firstI + 4) % 5, firstJ];
+                    second = Matrix[(secondI + 4) % 5, secondJ];
+                }
+                else
+                {
+                    first = Matrix[firstI, secondJ];
+                    second = Matrix[secondI, firstJ];
+                }
+
+                builder.AppendFormat("{0}{1}", first, second);
+            }
+
+            if (builder.Length % 2 == 0 && builder[^1] == 'x') builder.Remove(message.Length - 1, 1);
+            message = builder.ToString();
+        }
+        
+        private static void IndexOf(char symbol, out int i, out int j)
+        {
+            j = 0;
+            for (i = 0; i < 5; i++)
+            {
+                for (j = 0; j < 5; j++)
+                {
+                    if (Matrix[i,j] == symbol) return;
+                }
+            }
+        }
+    }
+}
---
+++ SecLab4/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SecLab4
+{
+    public static class Program
+    {
+        private const string FilePath = "/home/basicec/text.txt";
+
+        public static void Main(string[] args)
+        {
+            if (!File.Exists(FilePath))
+            {
+                Console.WriteLine($"Cannot find file {FilePath}");
+                return;
+            }
+
+            using var sr = new StreamReader(FilePath);
+            var message = sr.ReadToEnd();
+            Console.Write($"Message: {message}");
+
+            var bytes = Encoding.UTF8.GetBytes(message);
+            Encrypt(bytes);
+            Console.WriteLine($"Encrypted: {Encoding.UTF8.GetString(bytes)}");
+            Decrypt(bytes);
+            Console.WriteLine($"Decrypted: {Encoding.UTF8.GetString(bytes)}");
+        }
+
+        private static void Encrypt(byte[] message)
+        {
+            const string lsr1String = "111010010001011010010010010100101101001010001100100101011010010010011101001111";
+            const string lsr2String = "100100100101000100100010101001001010100101001010100010101101010010100001100111";
+            const string lsr3String = "10010010010010010010010100001001000101101010010001011010101001001001001011011101";
+    
+            var lsr1 = new BitArray(lsr1String.Select(_ => _ == '1').ToArray());
+            var lsr2 = new BitArray(lsr2String.Select(_ => _ == '1').ToArray());
+            var lsr3 = new BitArray(lsr3String.Select(_ => _ == '1').ToArray());
+
+            for (var i = 0; i < message.Length; i++)
+            {
+                var resultLetter = 0;
+
+                for (var mask = 0; mask < 8; mask++)
+                {
+                    var result = lsr2[0] && lsr3[0] ? 1 : 0;
+                    resultLetter |= result << mask;
+
+                    bool buff;
+                    if (!lsr1[24])
+                    {
+                        buff = lsr2[0] ^ lsr2[2] ^ lsr2[5] ^ lsr2[6] ^ lsr2[77];
+                        lsr2 = lsr2.RightShift(1);
+                        lsr2[77] = buff;
+                    }
+
+                    if (!lsr1[77])
+                    {
+                        buff = lsr3[0] ^ lsr3[2] ^ lsr3[3] ^ lsr3[4] ^ lsr2[77];
+                        lsr3 = lsr3.RightShift(1);
+                        lsr3[79] = buff;
+                    }
+
+                    buff = lsr1[0] ^ lsr1[1] ^ lsr1[3] ^ lsr1[6] ^ lsr1[75];
+                    lsr1 = lsr1.RightShift(1);
+                    lsr1[77] = buff;
+                }
+
+                message[i] ^= (byte) resultLetter;
+            }
+        }
+
+        private static void Decrypt(byte[] message)
+        {
+            Encrypt(message);
+        }
+    }
+}
